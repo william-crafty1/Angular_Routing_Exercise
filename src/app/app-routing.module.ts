06f7c2517fd0089aba7a9ed555bdc0ecf4289776
will c <will.cra@outlook.com>
2020-07-16 19:26:45 -0400

    Angular routing exercise, just the basics to understand the concept

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DogComponent } from './dog/dog.component';
+import { CatComponent } from './cat/cat.component';
 
 
-const routes: Routes = [];
+const catsAndDogsRoutes: Routes = [
+  {path: 'dog', component: DogComponent},
+  {path: '', redirectTo: '/dog', pathMatch:'full'},
+  {path: 'cat', component: CatComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(catsAndDogsRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DogComponent } from './dog/dog.component';
+import { CatComponent } from './cat/cat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DogComponent,
+    CatComponent
   ],
   imports: [
     BrowserModule,
